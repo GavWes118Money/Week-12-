1b15679a21aff6a069a5a116f78b60bc0eddebd5
GavWes118Money <Westry.G182924@stu.sanjac.edu>
2026-04-09 16:09:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+  const gallery = document.getElementById("gallery");
+  const box = document.querySelector(".box");
+  const colorButton = document.getElementById("changeColor");
+  const toggleButton = document.getElementById("toggleImage");
+ 
+ 
+  colorButton.addEventListener("click", () => {
+    bx.style.backgroundColor = "blue"; 
+  });
  
 
