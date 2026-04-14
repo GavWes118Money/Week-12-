2ac225915d2fecd16f94e37328f5d7eb519e52d8
GavWes118Money <Westry.G182924@stu.sanjac.edu>
2026-04-14 07:57:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,20 @@ document.addEventListener("DOMContentLoaded", () => {
     bx.style.backgroundColor = "blue"; 
   });
  
-
+ 
+ 
+  let toggled = false;
+  toggleButton.addEventListener("click", () => {
+    if (toggled) {
+      gallery.src = "./assets/image1.jpg";
+      toggled = false;
+    } else {
+      gallery.src = "./assets/image-does-not-exist.jpg"; 
+      toggled = true;
+    }
+  });
+ 
+ 
+ 
+  console.log("Page loaded!"
+});
